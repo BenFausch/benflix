5392033b7defcfccb59fea461ecfd623cee0a95e
ben fausch <benjamin.fausch@gmail.com>
2017-11-23 10:43:25 -0700

    updating findmovies to work with eggnoodles

--- findMovies.js
+++ findMovies.js
@@ -1,49 +1,60 @@
 var dirTree = require('directory-tree');
-
 console.log('BENFLIX STARTED')
-
 // buildDb();
 /////////
 var chokidar = require('chokidar');
-
 // One-liner for current directory, ignores .dotfiles
-chokidar.watch('videos')
-.on('change', (event, path) => {
-  // console.log(event, path);
-  console.log('FILES CHANGED YO');
-  buildDb();
-})
-.on('add', (event, path) => {
-  // console.log(event, path);
-  // console.log('FILES ADDED YO');
-  buildDb();
-});
-
+// chokidar.watch('D:')
+// .on('change', (event, path) => {
+//   // console.log(event, path);
+//   console.log('FILES CHANGED YO');
+//   buildDb();
+// })
+// .on('add', (event, path) => {
+//   // console.log(event, path);
+//   // console.log('FILES ADDED YO');
+//   buildDb();
+// });
 ///////
 // to run this file, run node findMovies.js
 ////////
-
-
-
-
-
-function buildDb(){
-var tree = dirTree('videos');
-tree = tree.children;
-
-var file = 'db.js';
-var jsonfile = require('jsonfile');
-
-jsonfile.writeFile(file, tree);
-
-
-
-var file = 'db2.js';
-var jsonfile = require('jsonfile');
-
-
-console.log('database updated.')
+function buildDb() {
+    var tree = dirTree('D:');
+    tree = tree.children;
+    //
+    function findAndReplace(object, value, replacevalue) {
+        for (var x in object) {
+            if (object.hasOwnProperty(x)) {
+                if (typeof object[x] == 'object') {
+                    findAndReplace(object[x], value, replacevalue);
+                } else {
+                    val = object[x]
+                    
+                    if(typeof val === 'string'){
+                    	console.log(val)	
+                    	val = val.replace(/D:/g, 'movies.benflix.com')
+                    	val = val.replace(/\\/g, '/')
+                    	object[x] = val
+                    	console.log(val)
+                    }
+                    
+                }
+                // if (object[x] == value) { 
+                //   // object[x].replace(/D:/g, 'movies.benflix.com')
+                //   object[x] = replacevalue;
+                //   // break; // uncomment to stop after first replacement
+                // }
+            }
+        }
+        return object
+    }
+    //
+    var newTree = findAndReplace(tree, 'D:', 'movies.benflix.com/');
+    var file = 'db.js';
+    var jsonfile = require('jsonfile');
+    jsonfile.writeFile(file, newTree);
+    // var file = 'db2.js';
+    // var jsonfile = require('jsonfile');
+    console.log('database updated.')
 };
-
-
-
+buildDb();
--- main.jsx
+++ main.jsx
@@ -15,12 +15,10 @@ var jsonFile = (function () {
     return json;
 })(); 
 var video = jsonFile;
-
-
 //Making the main component, InstantBox
+
 var InstantBox = React.createClass({
 	doSearch:function(queryText){
-	
 	if(queryText.length>0){
 		jQuery('#searchResult').empty().append('you are searching for ');
 		jQuery('#searched').empty().append(queryText);
@@ -28,16 +26,12 @@ var InstantBox = React.createClass({
 		jQuery('#searchResult').empty();
 		jQuery('#searched').empty();
 		};
-
-		
-		
 		//get query result
 		var queryResult=[];
 		this.props.data.forEach(function(video){
 			if(video.name.toLowerCase().indexOf(queryText)!=-1)
 			queryResult.push(video);
 		});
-		
 		this.setState({
 			query:queryText,
 			filteredData: queryResult
@@ -61,7 +55,6 @@ var InstantBox = React.createClass({
 		);
 	}
 });
-
 var SearchBox = React.createClass({
 	doSearch:function(){
 		var query=this.refs.searchInput.getDOMNode().value; // this is the search text
@@ -71,7 +64,6 @@ var SearchBox = React.createClass({
 		return <input type="text" ref="searchInput" placeholder="find something" value={this.props.query} onChange={this.doSearch}/>
 	}
 });
-
 var DisplayTable = React.createClass({
 	render:function(){
 		//making the rows to display
@@ -79,36 +71,37 @@ var DisplayTable = React.createClass({
 		var index = 0;
 		this.props.data.forEach(function(video) {
 		index++;
-		
-			var divStyle = '';
+			var divStyle = {
+		  					backgroundImage: 'url()'
+						};
 			var videoUrl = '';
 			var videoTitle = video.name;
 			var childTitle;
 			var grandchildren = [];
+			if(typeof(video['children'])!=='undefined'){
+				video['children'].forEach(function(child){
+					if(child.path.indexOf('.jpg')>=0){
+						divStyle = {
+							
+		  					backgroundImage: 'url(\'/' + child.path + '\')'
+						};
+					}
+					else if((child.path.indexOf('.mp4')>=0)){
+						videoUrl = child.path;
+					}
+					//get season
+					if(typeof(child['children'])!=='undefined'){
+						if(!(child.name.indexOf('.DS_Store')>=0)&&!(child.name.indexOf('.jpg')>=0)&&!(child.name.indexOf('.mp4')>=0)){
+											childTitle = child.name;
+											//get season children
+											child['children'].forEach(function(grandchild){
+												grandchildren.push(<li><a className="grandChild" href={grandchild.path}>{grandchild.name}</a></li>);
+											});
+										}
+						}
+				});
+			}
 
-
-			video['children'].forEach(function(child){
-
-				if(child.path.indexOf('.jpg')>=0){
-					divStyle = {
-	  					backgroundImage: 'url(\'/' + child.path + '\')'
-					};
-				}
-				else if((child.path.indexOf('.mp4')>=0)){
-					videoUrl = child.path;
-				}
-
-				//get season
-				if(!(child.name.indexOf('.DS_Store')>=0)&&!(child.name.indexOf('.jpg')>=0)&&!(child.name.indexOf('.mp4')>=0)){
-					childTitle = child.name;
-
-					//get season children
-					child['children'].forEach(function(grandchild){
-						grandchildren.push(<li><a className="grandChild" href={grandchild.path}>{grandchild.name}</a></li>);
-					});
-				}
-			});
-			
 
 		rows.push(
 			<div style={divStyle} id={'bf-' + index}>
@@ -125,15 +118,9 @@ var DisplayTable = React.createClass({
 		});
 		//returning the table
 		return(
-
 <div id="rowBox">
-
 {rows}</div>
-            
 		);
 	}
 });
-
- 
 React.renderComponent(<InstantBox data={jsonFile}/>,document.body);
-
